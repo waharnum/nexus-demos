61c2ccf0d30712715dfc3c91415c7996a78ac2ca
Simon Bates <simon@bitstructures.com>
2016-02-04 14:35:04 -0500

    NexusConnector with 2 inputs

--- AsTeRICS/Configure Nexus.ps1
+++ AsTeRICS/Configure Nexus.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest http://localhost:9081/components/example1 -Method Post -Headers @{"Content-Type" = "application/json"} -Body '{ "type": "fluid.modelComponent", "model": { "a": null } }'
+Invoke-WebRequest http://localhost:9081/components/asterics -Method Post -Headers @{"Content-Type" = "application/json"} -Body '{ "type": "fluid.modelComponent", "model": { "inputs": { "a": 0, "b": 0 } } }'
