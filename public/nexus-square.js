b3f5fac3b5f71a0c4a9bf2c1ab6ffeb75eaf10e1
Simon Bates <simon@bitstructures.com>
2016-02-05 15:46:30 -0500

    Add WebComponent example

---
+++ public/nexus-square.js
@@ -0,0 +1,34 @@
+document.registerElement("nexus-square", {
+    prototype: Object.create(HTMLElement.prototype, {
+        createdCallback: {
+            value: function () {
+                var astericsA = 0;
+                var astericsB = 0;
+
+                var websocket = new WebSocket("ws://localhost:9081/bindModel/asterics/inputs");
+                websocket.onmessage = function (evt) {
+                    var inputs = JSON.parse(evt.data);
+                    astericsA = inputs.a;
+                    astericsB = inputs.b;
+                };
+
+                new p5(function (p) {
+                    p.setup = function () {
+                        p.createCanvas(720, 720);
+                        p.colorMode(p.RGB, 100);
+                        p.rectMode(p.CENTER);
+                        p.noStroke();
+                    };
+
+                    p.draw = function () {
+                        var xPos = p.constrain(p.width/2 + astericsA, 0, p.width);
+                        var size = p.constrain(p.width/2 + astericsB/2, 10, p.width-10);
+                        p.background(20);
+                        p.fill(80);
+                        p.rect(xPos, p.height/2, size, size);
+                    };
+                }, this);
+            }
+        }
+    })
+});
