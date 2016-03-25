90abfae740e16cf8dba722e0d98d284fd7896be2
Simon Bates <simon@bitstructures.com>
2016-03-25 14:19:51 -0400

    GPII-1714: Use camel case

--- examples/bonang/construct_bonang-synth_peer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl \
-    --header 'Content-Type:application/json' \
-    --data '{ "type": "fluid.modelComponent", "model": { "activeNote": -1 } }' \
-    http://localhost:9081/components/bonang
