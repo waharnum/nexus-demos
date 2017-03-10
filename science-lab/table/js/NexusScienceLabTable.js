393056301ca280b9664d5ccc3a99e387f5927729
Simon Bates <simon@bitstructures.com>
2017-03-10 16:18:05 -0500

    GPII-2188: Render table

--- science-lab/table/js/NexusScienceLabTable.js
+++ science-lab/table/js/NexusScienceLabTable.js
@@ -11,17 +11,54 @@
             receivesChangesFromNexus: true,
             sendsChangesToNexus: false
         },
+        selectors: {
+            table: ".flc-nexus-science-lab-table",
+            tableHead: ".flc-nexus-science-lab-table-head",
+            tableBody: ".flc-nexus-science-lab-table-body"
+        },
+        listeners: {
+            "onCreate.buildTable": {
+                listener: "gpii.nexusScienceLabTable.buildTable",
+                args: ["{that}.dom.table"]
+            }
+        },
         modelListeners: {
-            sensorValues: [
-                "gpii.nexusScienceLabTable.log({change}.value)"
-            ]
+            sensorValues: {
+                funcName: "gpii.nexusScienceLabTable.updateTable",
+                args: ["{that}.dom.tableHead", "{that}.dom.tableBody", "{change}.value"]
+            }
         }
     });
 
-    // TODO: Render the sensor values in a table
+    gpii.nexusScienceLabTable.buildTable = function (tableElem) {
+        tableElem.html("<thead><tr class='flc-nexus-science-lab-table-head'></tr></thead><tbody><tr class='flc-nexus-science-lab-table-body'></tr></tbody>");
+    };
 
-    gpii.nexusScienceLabTable.log = function (sensorValues) {
-        console.log(JSON.stringify(sensorValues));
+    // TODO: Don't empty and rebuild the table for every update:
+    //       If a value is in the table, update it
+    //       If a value is not in the table, add it at the appropriate column
+    //       If a column is in the table but not in the data, remove it
+
+    gpii.nexusScienceLabTable.updateTable = function (tableHead, tableBody, sensorValues) {
+        var sensorValuesArray = fluid.hashToArray(
+            sensorValues,
+            "sensorName",
+            function (newElement, oldElement) {
+                newElement.sensorValue = oldElement;
+            }
+        );
+
+        fluid.stableSort(sensorValuesArray, function (a, b) {
+            return a.sensorName.localeCompare(b.sensorName);
+        });
+
+        tableHead.empty();
+        tableBody.empty();
+
+        fluid.each(sensorValuesArray, function (sensor) {
+            tableHead.append("<th>" + sensor.sensorName + "</th>");
+            tableBody.append("<td>" + sensor.sensorValue + "</td>");
+        });
     };
 
 }());
