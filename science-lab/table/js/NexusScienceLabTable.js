18900887ae3e593c150cf1234180191c512a7a63
Simon Bates <simon@bitstructures.com>
2017-03-14 17:33:24 -0400

    GPII-2188: Add a "No sensors connected" message

--- science-lab/table/js/NexusScienceLabTable.js
+++ science-lab/table/js/NexusScienceLabTable.js
@@ -13,63 +13,58 @@
             receivesChangesFromNexus: true,
             sendsChangesToNexus: false
         },
-        selectors: {
-            table: ".flc-nexus-science-lab-table",
-            tableHead: ".flc-nexus-science-lab-table-head",
-            tableBody: ".flc-nexus-science-lab-table-body"
-        },
-        listeners: {
-            "onCreate.buildTable": {
-                listener: "gpii.nexusScienceLabTable.buildTable",
-                args: ["{that}.dom.table"]
-            }
+        strings: {
+            noSensorsConnected: "No sensors connected"
         },
         modelListeners: {
             sensors: {
                 funcName: "gpii.nexusScienceLabTable.updateTable",
                 args: [
-                    "{that}.dom.tableHead",
-                    "{that}.dom.tableBody",
+                    "{that}.container",
                     "{that}.options.numberLocale",
                     "{that}.options.maximumFractionDigits",
+                    "{that}.options.strings.noSensorsConnected",
                     "{change}.value"
                 ]
             }
         }
     });
 
-    gpii.nexusScienceLabTable.buildTable = function (tableElem) {
-        tableElem.html("<thead><tr class='flc-nexus-science-lab-table-head'></tr></thead><tbody><tr class='flc-nexus-science-lab-table-body'></tr></tbody>");
-    };
-
-    // TODO: Don't empty and rebuild the table for every update:
+    // TODO: Don't rebuild the table for every update:
     //       If a value is in the table, update it
     //       If a value is not in the table, add it at the appropriate column
     //       If a column is in the table but not in the data, remove it
 
-    gpii.nexusScienceLabTable.updateTable = function (tableHead, tableBody, numberLocale, maximumFractionDigits, sensors) {
+    gpii.nexusScienceLabTable.updateTable = function (container, numberLocale, maximumFractionDigits, noSensorsConnectedMessage, sensors) {
         var sensorsArray = fluid.hashToArray(
             sensors,
             "sensor"
         );
 
-        fluid.stableSort(sensorsArray, function (a, b) {
-            return a.name.localeCompare(b.name);
-        });
-
-        tableHead.empty();
-        tableBody.empty();
-
-        fluid.each(sensorsArray, function (sensor) {
-            tableHead.append(fluid.stringTemplate("<th>%sensorName</th>", {
-                sensorName: sensor.name
+        if (sensorsArray.length === 0) {
+            container.html(fluid.stringTemplate("<p>%message</p>", {
+                message: noSensorsConnectedMessage
             }));
-            tableBody.append(fluid.stringTemplate("<td>%sensorValue</td>", {
-                sensorValue: sensor.value.toLocaleString(numberLocale, {
-                    maximumFractionDigits: maximumFractionDigits
-                })
-            }));
-        });
+        } else {
+            fluid.stableSort(sensorsArray, function (a, b) {
+                return a.name.localeCompare(b.name);
+            });
+
+            container.html("<table><thead><tr class='flc-nexus-science-lab-table-head'></tr></thead><tbody><tr class='flc-nexus-science-lab-table-body'></tr></tbody></table>");
+            var tableHead = container.find(".flc-nexus-science-lab-table-head");
+            var tableBody = container.find(".flc-nexus-science-lab-table-body");
+
+            fluid.each(sensorsArray, function (sensor) {
+                tableHead.append(fluid.stringTemplate("<th>%sensorName</th>", {
+                    sensorName: sensor.name
+                }));
+                tableBody.append(fluid.stringTemplate("<td>%sensorValue</td>", {
+                    sensorValue: sensor.value.toLocaleString(numberLocale, {
+                        maximumFractionDigits: maximumFractionDigits
+                    })
+                }));
+            });
+        }
     };
 
 }());
