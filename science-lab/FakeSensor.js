4af03cd38ba7c711865b11d0117a18fe1b71592d
Alan Harnum <aharnum@ocadu.ca>
2017-03-17 12:05:09 -0400

    GPII-2190: add a fake pH sensor definition.

--- science-lab/FakeSensor.js
+++ science-lab/FakeSensor.js
@@ -53,6 +53,23 @@ fluid.defaults("gpii.nexus.fakeSensor.sinValue", {
     }
 });
 
+fluid.defaults("gpii.nexus.fakeSensor.pHValue", {
+    gradeNames: ["gpii.nexus.fakeSensor"],
+    model: {
+        sensorData: {
+            name: "Fake pH Sensor",
+            rangeMin: 0,
+            rangeMax: 14,
+            value: 7
+        }
+    },
+        invokers: {
+        "getFakeSensorValue": {
+            "funcName": "gpii.nexus.fakeSensor.getFakeSensorValuePH"
+        }
+    }
+});
+
 gpii.nexus.fakeSensor.exitProcess = function () {
     process.exit();
 };
@@ -69,6 +86,21 @@ gpii.nexus.fakeSensor.update = function (that) {
 // A fairly even sin-based sensor value that moves between -1 and 1
 gpii.nexus.fakeSensor.getFakeSensorValueSin = function () {
     return Math.sin((new Date().getTime() % sinPeriodMs) * Math.PI * 2 / sinPeriodMs);
+};
+
+// a pH sensor type value between 0 and 14
+gpii.nexus.fakeSensor.getFakeSensorValuePH = function () {
+    return gpii.nexus.fakeSensor.randomFloat(0,14);
+};
+
+gpii.nexus.fakeSensor.randomInt = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+gpii.nexus.fakeSensor.randomFloat = function(min, max) {
+    return Math.random() * (max - min) + min;
 };
 
 var sensor = gpii.nexus.fakeSensor.sinValue();
