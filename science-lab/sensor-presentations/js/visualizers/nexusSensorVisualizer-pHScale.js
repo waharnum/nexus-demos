b6eae1b47c2be842c1349ad9d922880622f9aefc
Alan Harnum <aharnum@ocadu.ca>
2017-04-12 17:14:53 -0400

    GPII-2189: pH scale color update.

--- science-lab/sensor-presentations/js/visualizers/nexusSensorVisualizer-pHScale.js
+++ science-lab/sensor-presentations/js/visualizers/nexusSensorVisualizer-pHScale.js
@@ -45,7 +45,7 @@
             textOptions: {
                 // Creates labels for each point of the scale
                 labels: {
-                    template: "Value %bandStart - %bandEnd",
+                    template: "Value %bandStart – %bandEnd",
                     // Scales the font size relative to the
                     // bar height - this may need tweaking
                     // dependent on the number of bars and
@@ -340,12 +340,14 @@
         scaleOptions: {
             min: 0,
             max: 14,
+            colors:
+            ["#fe0002", "#ff6600", "#f8c50a", "#ffff01", "#b2fb0c", "#60fe2f", "#02de00", "#35a43b", "#00b46b", "#00b9b4", "#0099ff", "#0000fe", "#5d05fa", "#6600cd", "#440089"],
             // This set generated using the tool at https://gka.github.io/palettes/
-            colors: ["#ff0000","#ff7100","#f49b00","#d9b100","#b3b500","#81ab00","#409200","#3a7539","#576071","#604b95","#6636a8","#6e20ab","#78079d","#800080"],
+            //  ["#ff0000","#ff7100","#f49b00","#d9b100","#b3b500","#81ab00","#409200","#3a7539","#576071","#604b95","#6636a8","#6e20ab","#78079d","#800080"],
             textOptions: {
                 // Creates labels for each point of the scale
                 labels: {
-                    template: "pH Value %bandStart - %bandEnd",
+                    template: "pH Value %bandStart – %bandEnd",
                     labelTextScalingToBarHeight: 0.5,
                     valueDecimalPlaces: 0
                 },
