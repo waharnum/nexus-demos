da97dcb3ef4994d8a2c6277614aa3f9c5bdff09c
Alan Harnum <aharnum@ocadu.ca>
2017-06-15 17:26:49 -0400

    GPII-2189: start implementing IoC.

--- science-lab/sensor-presentations/tests/js/nexusSensorVisualizersTests.js
+++ science-lab/sensor-presentations/tests/js/nexusSensorVisualizersTests.js
@@ -4,6 +4,15 @@
 
     "use strict";
 
+    fluid.defaults("gpii.tests.visualizerTestsBase", {
+        gradeNames: ["fluid.test.testEnvironment"],
+        components: {
+            visualizerTester: {
+                type: "gpii.tests.visualizerTester"
+            }
+        }
+    });
+
     fluid.defaults("gpii.tests.testVisualizerBase", {
         gradeNames: ["gpii.nexusSensorVisualizerBase"],
         components: {
@@ -26,6 +35,43 @@
         }
     });
 
+    fluid.defaults("gpii.tests.realTimeVisualizerTests", {
+        gradeNames: ["gpii.tests.visualizerTestsBase"],
+        components: {
+            visualizerTester: {
+                type: "gpii.tests.visualizerTester"
+            },
+            sensorVisualizer: {
+                type: "gpii.tests.testRealTimeVisualizer",
+                createOnEvent: "{visualizerTester}.events.onTestCaseStart"
+            }
+        }
+    });
+
+    fluid.defaults("gpii.tests.visualizerTester", {
+        gradeNames: ["fluid.test.testCaseHolder"],
+        modules: [{
+            name: "Test real-time visualizer",
+            tests: [{
+                name: "Test presentation panel sensor create and remove behaviour",
+                expect: 1,
+                sequence: [{
+                    func: "gpii.tests.verifyIndicator",
+                    args: ["{sensorVisualizer}.visualizer.dom.sensorValueIndicator", "height", "230"]
+                }]
+            }]
+        }]
+    });
+
+    fluid.defaults("gpii.tests.testRealTimeVisualizer", {
+        gradeNames: ["gpii.nexusSensorVisualizer.realTimeScale", "gpii.tests.testVisualizerBase"],
+        components: {
+            visualizer: {
+                container: "#visualizer-realTimeScale"
+            }
+        }
+    });
+
     gpii.tests.mockSensorVisualizerFactory = function (sensorVisualizerGrade, visualizerContainer, visualizerOptions) {
 
         return sensorVisualizerGrade({
@@ -40,35 +86,28 @@
     };
 
     gpii.tests.verifyIndicator = function (indicator, checkAttribute, expectedValue) {
-        console.log(indicator);
         var message = fluid.stringTemplate("Attribute '%checkAttribute' is expected value of %expectedValue", {checkAttribute: checkAttribute, expectedValue: expectedValue});
         jqUnit.assertEquals(message, expectedValue, indicator.attr(checkAttribute));
     };
 
-    jqUnit.test("Test real-time scale", function() {
-        var realTimeScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.realTimeScale, "#visualizer-realTimeScale");
+    gpii.tests.realTimeVisualizerTests();
 
-        gpii.tests.verifyIndicator(realTimeScale.visualizer.locate("sensorValueIndicator"), "height", "230");
-
-        realTimeScale.sensor.applier.change("sensorValue", 100);
-
-        gpii.tests.verifyIndicator(realTimeScale.visualizer.locate("sensorValueIndicator"), "height", "460");
-
-    });
-
-    jqUnit.test("Test color scale", function() {
-        var colorScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.colorScale, "#visualizer-colorScale", {scaleOptions: {colors: ["#FF0000","#00FF00", "#0000FF"]}});
-
-        gpii.tests.verifyIndicator(colorScale.visualizer.locate("indicator"), "transform", "translate(40, 235)");
-    });
-
-    $(document).ready(function () {
-
-        // var circularPercentageScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.circleRadius, "#visualizer-circularPercentageScale");
-        //
-        // var horizontalBarPercentageScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.horizontalBar, "#visualizer-horizontalBarPercentageScale");
-        //
-        // var colorScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.colorScale, "#visualizer-colorScale", {scaleOptions: {colors: ["#FF0000","#00FF00", "#0000FF"]}});
-    })
+    // jqUnit.test("Test real-time scale", function() {
+    //     var realTimeScale = gpii.tests.testRealTimeVisualizer();
+    //     console.log(realTimeScale);
+    //
+    //     gpii.tests.verifyIndicator(realTimeScale.visualizer.locate("sensorValueIndicator"), "height", "230");
+    //
+    //     realTimeScale.sensor.applier.change("sensorValue", 100);
+    //
+    //     gpii.tests.verifyIndicator(realTimeScale.visualizer.locate("sensorValueIndicator"), "height", "460");
+    //
+    // });
+    //
+    // jqUnit.test("Test color scale", function() {
+    //     var colorScale = gpii.tests.mockSensorVisualizerFactory(gpii.nexusSensorVisualizer.colorScale, "#visualizer-colorScale", {scaleOptions: {colors: ["#FF0000","#00FF00", "#0000FF"]}});
+    //
+    //     gpii.tests.verifyIndicator(colorScale.visualizer.locate("indicator"), "transform", "translate(40, 235)");
+    // });
 
 }());
